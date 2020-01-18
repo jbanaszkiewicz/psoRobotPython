375cf460b51f1150a18b2289e315600d314a5c3a
jbanaszkiewicz <jakub.banaszkiewicz@gmail.com>
2020-01-18 14:23:53 +0100

    Start code pso

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import graph
 import path
-
+from copy import deepcopy
 
 
 nrParticles = 2
 filename = r'./graphs/graph100'
 nrNodes, neighborhood, nodes = graph.loadGraph(filename)
 edges = graph.generateEdges(nodes, neighborhood)
+
 randomPaths = path.getRandomPaths(edges, nodes, nrParticles)
+bestPaths = deepcopy(randomPaths)
+bestPathGlobal = deepcopy(bestPaths[0])
+
+
+##pso search
 
---
+++ pso.py
@@ -0,0 +1,34 @@
+import numpy as np
+#wybrac najlepsza globalnie
+#wybrac najlepsza sciezke dla Particle
+#porwnac aktulną losową z tymi dwoma
+def findBest(paths, nodes, edges):
+    pass
+def generateParticles():
+    particle = ()
+
+def countCost(path):
+
+    length = 0
+    partialLength = 0
+
+    for idx in range(len(path)-1):
+        
+  for (auto i = 1; i < ; i++)
+  {
+    currentX = this->nodes.at(i)->getPositionX();
+    currentY = this->nodes.at(i)->getPositionY();
+
+    partialLength = sqrt( pow( currentX - previousX, 2 ) + pow( currentY - previousY, 2 ) );
+    length += partialLength;
+
+    if (i != (this->nodes.size() - 1))
+    {
+      previousX = currentX;
+      previousY = currentY;
+    }
+  }
+
+  return length;
+}
+
